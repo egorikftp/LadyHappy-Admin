4e5441df031d23f6b9a6ebce13f6d2fc075965ae
yahor_urbanovich <egorikftp@gmail.com>
2022-03-03 12:00:09 +0300

    Update module structure

---
+++ desktop/build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose") version "1.1.0-rc01"
+}
+
+group = "com.egoriku.ladyhappy"
+version = "1.0"
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+        withJava()
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+            }
+        }
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "Ladyhappy-Admin"
+            packageVersion = "1.0.0"
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,14 @@ pluginManagement {
     repositories {
         google()
         gradlePluginPortal()
+        mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
-    
 }
+
+enableFeaturePreview("VERSION_CATALOGS")
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 rootProject.name = "Ladyhappy-Admin"
 
+include(":desktop")
