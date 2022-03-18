ff9bf9137e846cde1e72079160fdff886203a918
yahor_urbanovich <egorikftp@gmail.com>
2022-03-18 13:32:27 +0500

    Apply material theme

--- common/theme/src/commonMain/kotlin/com/egoriku/theme/Theme.kt
+++ common/theme/src/commonMain/kotlin/com/egoriku/theme/Theme.kt
@@ -5,17 +5,18 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color(0xFF1F0318),
+    secondary = Color(0xFF8C705F),
+    surface = Color(0xFF1E1A1D)
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color(0xFF1F0318),
+    secondary = Color(0xFF8C705F),
+    surface = Color.White
 )
 
 @Composable
@@ -23,10 +24,9 @@ fun LadyHappyAdminTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
+    val colors = when {
+        darkTheme -> DarkColorPalette
+        else -> LightColorPalette
     }
 
     MaterialTheme(
