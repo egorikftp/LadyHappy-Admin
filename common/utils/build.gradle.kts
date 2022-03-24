1e4322217a8576428bb91cd4f9e1b62e92b8dd21
yahor_urbanovich <egorikftp@gmail.com>
2022-03-24 21:25:23 +0500

    Remove viewmodel module, introduce utils with coroutine dispatchers

---
+++ common/utils/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("org.jetbrains.compose")
+}
+
+android {
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    sourceSets {
+        named("main") {
+            manifest.srcFile("src/androidMain/AndroidManifest.xml")
+            res.srcDirs("src/androidMain/res")
+        }
+    }
+}
+
+kotlin {
+    jvm()
+    android()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.kotlin.coroutines.core)
+                implementation(compose.desktop.common)
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+                implementation(libs.kotlin.coroutines.android)
+            }
+        }
+    }
+}
