e0d8045a84a8b7c59cb0d100f52cfcf321a2c1c2
yahor_urbanovich <egorikftp@gmail.com>
2022-04-01 15:48:55 +0500

    Skip empty columns during parsing csv

--- common/retrosheet-kmm/src/jvmMain/kotlin/com/egoriku/retrosheetkmm/CsvParser.kt
+++ common/retrosheet-kmm/src/jvmMain/kotlin/com/egoriku/retrosheetkmm/CsvParser.kt
@@ -3,5 +3,9 @@ package com.egoriku.retrosheetkmm
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 
 actual fun parseCsv(data: String): List<Map<String, String>> {
-    return csvReader().readAllWithHeader(data)
+    return csvReader()
+        .readAllWithHeader(data)
+        .map { map ->
+            map.filterKeys { key -> key.isNotEmpty() }
+        }
 }
