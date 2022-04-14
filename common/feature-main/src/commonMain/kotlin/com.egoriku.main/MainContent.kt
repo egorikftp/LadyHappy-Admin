f68b05f5ac7a872135a545efbb71c11305b55ae4
yahor_urbanovich <egorikftp@gmail.com>
2022-04-14 14:36:45 +0300

    Update to decompose 0.6.0

--- common/feature-main/src/commonMain/kotlin/com.egoriku.main/MainContent.kt
+++ common/feature-main/src/commonMain/kotlin/com.egoriku.main/MainContent.kt
@@ -3,15 +3,19 @@ package com.egoriku.main
 import androidx.compose.runtime.Composable
 import com.arkivanov.decompose.ExperimentalDecomposeApi
 import com.arkivanov.decompose.extensions.compose.jetbrains.Children
-import com.arkivanov.decompose.extensions.compose.jetbrains.animation.child.crossfadeScale
+import com.arkivanov.decompose.extensions.compose.jetbrains.animation.child.childAnimation
+import com.arkivanov.decompose.extensions.compose.jetbrains.animation.child.fade
+import com.arkivanov.decompose.extensions.compose.jetbrains.animation.child.plus
+import com.arkivanov.decompose.extensions.compose.jetbrains.animation.child.scale
 import com.egoriku.config.ConfigContent
 import com.egoriku.features.FeaturesContent
-import com.egoriku.main.MainComponent.Child.*
+import com.egoriku.main.MainComponent.Child.Config
+import com.egoriku.main.MainComponent.Child.Features
 
 @OptIn(ExperimentalDecomposeApi::class)
 @Composable
 fun MainContent(component: MainComponent) {
-    Children(routerState = component.routerState, animation = crossfadeScale()) {
+    Children(routerState = component.routerState, animation = childAnimation(scale() + fade())) {
         when (val child = it.instance) {
             is Config -> ConfigContent(child.component)
             is Features -> FeaturesContent(child.component)
--- common/feature-root/src/commonMain/kotlin/com/egoriku/root/RootContent.kt
+++ common/feature-root/src/commonMain/kotlin/com/egoriku/root/RootContent.kt
@@ -6,7 +6,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.arkivanov.decompose.ExperimentalDecomposeApi
 import com.arkivanov.decompose.extensions.compose.jetbrains.Children
-import com.arkivanov.decompose.extensions.compose.jetbrains.animation.child.crossfadeScale
+import com.arkivanov.decompose.extensions.compose.jetbrains.animation.child.childAnimation
+import com.arkivanov.decompose.extensions.compose.jetbrains.animation.child.fade
+import com.arkivanov.decompose.extensions.compose.jetbrains.animation.child.plus
+import com.arkivanov.decompose.extensions.compose.jetbrains.animation.child.scale
 import com.egoriku.main.MainContent
 import com.egoriku.root.RootComponent.Child
 
@@ -14,7 +17,10 @@ import com.egoriku.root.RootComponent.Child
 @Composable
 fun RootContent(rootComponent: RootComponent) {
     Surface(modifier = Modifier.fillMaxSize()) {
-        Children(routerState = rootComponent.routerState, animation = crossfadeScale()) {
+        Children(
+            routerState = rootComponent.routerState,
+            animation = childAnimation(scale() + fade())
+        ) {
             when (val child = it.instance) {
                 is Child.Main -> MainContent(component = child.component)
                 //is Child.Login -> ConfigContent(component = child.component)
