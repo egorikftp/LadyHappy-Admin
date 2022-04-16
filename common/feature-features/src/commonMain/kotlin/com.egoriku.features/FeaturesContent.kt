fb6bf85d7a88a2302da29252b8aaab80e0dbd03f
yahor_urbanovich <egorikftp@gmail.com>
2022-04-16 12:34:56 +0300

    Make scalable FeatureContent screen

--- common/feature-features/src/commonMain/kotlin/com.egoriku.features/FeaturesContent.kt
+++ common/feature-features/src/commonMain/kotlin/com.egoriku.features/FeaturesContent.kt
@@ -1,8 +1,12 @@
 package com.egoriku.features
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
@@ -10,40 +14,33 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.egoriku.utils.screen.WindowSize
+import com.egoriku.utils.screen.rememberWindowSize
 
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
 @Composable
 fun FeaturesContent(featuresComponent: FeaturesComponent) {
     val state by featuresComponent.model.collectAsState(FeaturesComponent.Model())
 
-    LazyColumn {
+    val gridCount = when (rememberWindowSize()) {
+        WindowSize.Compact -> 1
+        WindowSize.Medium -> 2
+        WindowSize.Expanded -> 3
+    }
+
+    LazyVerticalGrid(
+        modifier = Modifier.fillMaxSize(),
+        cells = GridCells.Fixed(gridCount),
+        contentPadding = PaddingValues(16.dp)
+    ) {
         items(state.features) {
-            Text(text = it.sheetName)
-        }
-    }
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        FeatureCard(
-            modifier = Modifier.align(Alignment.Center),
-            name = "Config",
-            onClick = featuresComponent::onFeatureClick
-        )
-    }
-}
-
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun FeatureCard(modifier: Modifier, name: String, onClick: () -> Unit) {
-    Card(modifier = modifier.fillMaxSize(fraction = 0.5f), onClick = onClick, elevation = 10.dp) {
-        Box {
-            Text(
-                text = name,
-                modifier = Modifier.align(Alignment.Center),
-                fontWeight = FontWeight.Bold
-            )
+            Card(modifier = Modifier.padding(16.dp), elevation = 4.dp, onClick = {}) {
+                Column(modifier = Modifier.padding(40.dp)) {
+                    Text(text = it.sheetName)
+                }
+            }
         }
     }
 }
